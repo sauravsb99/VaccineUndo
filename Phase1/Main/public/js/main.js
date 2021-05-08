356758b492599130e7150ea94cb826d16bda8bde
adarsh-av13 <dra4474@gmail.com>
2021-05-08 14:56:01 +0530

    converted to ejs

--- Phase1/Main/public/js/main.js
+++ Phase1/Main/public/js/main.js
@@ -1,16 +1,16 @@
-(function ($) {
+(function($) {
     // USE STRICT
     "use strict";
 
-    $(".form-radio .radio-item").click(function(){
+    $(".form-radio .radio-item").click(function() {
         //Spot switcher:
         $(this).parent().find(".radio-item").removeClass("active");
         $(this).addClass("active");
     });
-  
+
     $('#time').parent().append('<ul class="list-item" id="newtime" name="time"></ul>');
-    $('#time option').each(function(){
-        $('#newtime').append('<li value="' + $(this).val() + '">'+$(this).text()+'</li>');
+    $('#time option').each(function() {
+        $('#newtime').append('<li value="' + $(this).val() + '">' + $(this).text() + '</li>');
     });
     $('#time').remove();
     $('#newtime').attr('id', 'time');
@@ -20,8 +20,8 @@
     });
 
     $('#food').parent().append('<ul class="list-item" id="newfood" name="food"></ul>');
-    $('#food option').each(function(){
-        $('#newfood').append('<li value="' + $(this).val() + '">'+$(this).text()+'</li>');
+    $('#food option').each(function() {
+        $('#newfood').append('<li value="' + $(this).val() + '">' + $(this).text() + '</li>');
     });
     $('#food').remove();
     $('#newfood').attr('id', 'food');
@@ -29,7 +29,7 @@
     $("#food").on("click", ".init", function() {
         $(this).closest("#food").children('li:not(.init)').toggle();
     });
-    
+
     var allOptions = $("#time").children('li:not(.init)');
     $("#time").on("click", "li:not(.init)", function() {
         allOptions.removeClass('selected');
